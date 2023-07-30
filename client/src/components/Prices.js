5bcfc2b081e3c9accb2536cb6be92b06fb9afb73
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-07-30 09:51:14 +0530

    Created Prices object

---
+++ client/src/components/Prices.js
@@ -0,0 +1,37 @@
+export const prices = [
+  {
+    _id: 0,
+    name: "$0 to $19",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "$20 to $29",
+    array: [2, 29],
+  },
+  {
+    _id: 2,
+    name: "$30 to $49",
+    array: [30, 49],
+  },
+  {
+    _id: 3,
+    name: "$50 to $69",
+    array: [50, 69],
+  },
+  {
+    _id: 4,
+    name: "$70 to $89",
+    array: [70, 89],
+  },
+  {
+    _id: 5,
+    name: "$90 to $99",
+    array: [90, 99],
+  },
+  {
+    _id: 6,
+    name: "More than $100",
+    array: [100, 9999999],
+  },
+];
