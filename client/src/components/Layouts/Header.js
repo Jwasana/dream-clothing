0687416b78db62690d696c999873521e9c0da1f3
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-02 11:11:52 +0530

    Add search bar into navbar

--- client/src/components/Layouts/Header.js
+++ client/src/components/Layouts/Header.js
@@ -3,6 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import "../../index.css";
+import SearchInput from "../Forms/SearchInput";
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
@@ -27,6 +28,7 @@ const Header = () => {
           </Link>
           {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <SearchInput />
             <li className="nav-item">
               <NavLink to="/" className="nav-link">
                 Home
