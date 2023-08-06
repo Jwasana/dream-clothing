3b895d33a8b99385cf24a6d420b4b782b204dfa8
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-07 04:13:03 +0530

    created product details page

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -6,8 +6,10 @@ import { useParams } from "react-router-dom";
 const ProductDetails = () => {
   const params = useParams();
   const [product, setProduct] = useState({});
+  const [similarProducts, setSimilarProducts] = useState([]);
   useEffect(() => {
     if (params?.slug) getProduct();
+    // eslint-disable-next-line
   }, [params?.slug]);
   //get product
   const getProduct = async () => {
@@ -16,10 +18,28 @@ const ProductDetails = () => {
         `/api/v1/product/single-product/${params.slug}`
       );
       setProduct(productData?.singleProduct);
+      getSimilarProducts(
+        productData?.singleProduct?._id,
+        productData?.singleProduct?.category?._id
+      );
     } catch (error) {
       console.log(error);
     }
   };
+
+  //get similar products
+  const getSimilarProducts = async (pid, cid) => {
+    try {
+      const { data } = await axios.get(
+        `/api/v1/product/related-product/${pid}/${cid}`
+      );
+      setSimilarProducts(data?.products);
+      console.log(222, similarProducts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout>
       <div className="row container mt-2">
@@ -29,7 +49,7 @@ const ProductDetails = () => {
             className="card-img-top"
             alt={product?.name}
             height="300"
-            width="250"
+            width="200"
           />
         </div>
         <div className="col-md-6 mt-5">
@@ -41,7 +61,35 @@ const ProductDetails = () => {
           <button className="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
       </div>
-      <div className="row">silmilar products</div>
+      <hr />
+      <div className="row container">
+        {similarProducts?.length < 1 && (
+          <p className="text-center">No Similar Products found</p>
+        )}
+
+        <h3>Similar Products</h3>
+        {/* {JSON.stringify(similarProducts, null, 4)} */}
+        <div className="d-flex flex-wrap">
+          {similarProducts?.map((product) => (
+            <div className="card m-2" style={{ width: "18rem" }}>
+              <img
+                src={`/api/v1/product/product-photo/${product._id}`}
+                className="card-img-top"
+                alt={product.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">
+                  {product.description.substring(0, 20)}...
+                </p>
+                <p className="card-text"> ${product.price}</p>
+
+                <button className="btn btn-secondary ms-1">ADD TO CART</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
