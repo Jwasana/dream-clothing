98f5b73f2cd262ccbb19817a54711b13d163c181
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-09 13:04:55 +0530

    update profile form

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layouts/Layout";
 import UserMenu from "../../components/Layouts/UserMenu";
+import { useAuth } from "../../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Profile = () => {
+  //context
+  const [auth, setAuth] = useAuth();
+
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //get user data
+  useEffect(() => {
+    const { name, email, phone, address } = auth?.user;
+    setName(name);
+    setEmail(email);
+    setPhone(phone);
+    setAddress(address);
+  }, [auth?.user]);
+
+  //form login function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put("/api/v1/auth/profile", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (data?.error) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data?.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        toast.success("Profile updated successfully!");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
+    }
+  };
+
   return (
     <Layout title={"Your Profile"}>
       <div className="container-fluid p-3 m-3">
@@ -11,7 +60,76 @@ const Profile = () => {
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <h1>Your Profile</h1>
+            <div className="form-container">
+              <form onSubmit={handleSubmit}>
+                <h4 className="title">USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    id="exampleInputName"
+                    placeholder="Enter Your Name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail"
+                    placeholder="Enter Your Email"
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Your Password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPhone"
+                    placeholder="Enter Your Phone"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputAddress"
+                    placeholder="Enter your Adress"
+                  />
+                </div>
+                {/* <div className="mb-3">
+                  <input
+                    type="text"
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                    className="form-control"
+                    id="exampleInputAnswer"
+                    placeholder="What is your favorite fruit?"
+                    required
+                  />
+                </div> */}
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
