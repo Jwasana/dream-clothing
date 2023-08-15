3d38db1696b280f66192925608367c060a65cc5e
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-15 20:27:21 +0530

    created get orders controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
+import orderModel from "../models/orderModel.js";
 
 export const registerController = async (req, res) => {
   try {
@@ -205,6 +206,24 @@ export const updateProfileController = async (req, res) => {
       success: false,
       message: "Error while updating profile!",
       error,
+    });
+  }
+};
+
+//orders
+export const getOrdersContrller = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req?.user?._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while etting orders",
     });
   }
 };
