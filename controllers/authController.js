bcd68326297f4cd3774289978ff147b40e2f6fbc
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-17 18:18:05 +0530

    update order status controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -228,6 +228,41 @@ export const getOrdersContrller = async (req, res) => {
   }
 };
 
+//get all oredrs controller
+export const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate("products", "-photo")
+      .populate("buyer", "name")
+      .sort({ createdAt: "-1" });
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update order status controller
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while updating status",
+      error,
+    });
+  }
+};
+
 //test controller
 export const testController = (req, res) => {
   console.log("Protected Route");
