b1877abf337897489cab348f9e1148c1bea14fd8
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2023-08-18 22:18:18 +0530

    fixed es-module error

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
@@ -16,6 +17,10 @@ const app = express();
 
 //database connect
 connectDB();
+
+//fix-esmodule
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(cors());
